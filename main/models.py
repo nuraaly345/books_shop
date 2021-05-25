d427579899a368fb4a62db6d5a376550390a2b31
nuraaly345 <kubanura57@gmail.com>
2021-05-25 18:55:43 +0300

    added todo models and admin

--- main/models.py
+++ main/models.py
@@ -1,3 +1,13 @@
+from enum import auto
+from typing import Text
 from django.db import models
 
-# Create your models here.
+class ToDo(models.Model):
+    title=models.CharField(max_length=100)
+    subtitle=models.CharField(max_length=200)
+    description=models.TextField()
+    price=models.DecimalField(max_digits=5, decimal_places=2)
+    genre=models.CharField(max_length=50)
+    author=models.CharField(max_length=50)
+    year=models.IntegerField()
+    date=models.DateTimeField(auto_now_add=True)
--- main/views.py
+++ main/views.py
@@ -1,7 +1,11 @@
-from django.shortcuts import render
+from typing import Text
+from django.shortcuts import render,redirect
+from .models import ToDo
 
 def home(request):
-    return render(request, 'book.html')
+    todo_list=ToDo.objects.all()
+    return render(request, 'book.html', {"todo_list": todo_list})
 
 def test(request):
+    
     return render(request, 'index.html')
